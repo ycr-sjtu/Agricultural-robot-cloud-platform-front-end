807d54f57bd2716cb41b93e4335fc5738c77e3f1
‘we89’ <2695307109@qq.com>
2023-04-21 21:19:22 +0800

    修改Aside

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
     <!--    默认打开第一个导航栏-->
-    <el-menu style="width: 200px; min-height: calc(100vh - 50px)" default-active="/home" router class="el-menu-vertical-demo">
+    <el-menu style="width: 250px; min-height: calc(100vh - 50px)" default-active="/home" router class="el-menu-vertical-demo">
       <el-menu-item index="/home">
         <el-icon>
           <IconMenu />
         </el-icon>
-        <span>首页展示</span>
+        <span>首页</span>
+      </el-menu-item>
+      <el-menu-item index="/setting">
+        <el-icon>
+          <IconMenu />
+        </el-icon>
+        <span>全国农机化信息服务平台</span>
       </el-menu-item>
       <el-sub-menu index="1">
         <template #title>
           <el-icon>
             <IconMenu />
           </el-icon>
-          <span>系统管理</span>
+          <span>田间蔬菜移栽物联网监控系统</span>
         </template>
         <el-menu-item index="/robot"><el-icon>
             <Document />
@@ -28,11 +34,30 @@
           </span></el-menu-item>
       </el-sub-menu>
       <el-menu-item index="/setting">
+      <el-icon>
+        <IconMenu />
+      </el-icon>
+      <span>智慧农业可视化管控平台</span>
+    </el-menu-item>
+
+      <el-menu-item index="/setting">
         <el-icon>
-          <setting />
+          <IconMenu />
         </el-icon>
-        <span>设置</span>
+        <span>智慧农场农机装备云管控系统</span>
       </el-menu-item>
+      <el-menu-item index="/setting">
+        <el-icon>
+          <IconMenu />
+        </el-icon>
+        <span>智能温室追光系统管控云平台</span>
+      </el-menu-item>
+<!--      <el-menu-item index="/setting">-->
+<!--        <el-icon>-->
+<!--          <setting />-->
+<!--        </el-icon>-->
+<!--        <span>设置</span>-->
+<!--      </el-menu-item>-->
     </el-menu>
   </div>
 </template>
