20b22634c815a035ca0fc638de0952496acee177
‘we89’ <2695307109@qq.com>
2023-04-23 21:24:35 +0800

    实现了地图实时显示最新一列的点（最多50个，最少0个）

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,38 +1,43 @@
 <template>
   <div style="display: flex;flex-direction: column;">
     <div class="content">
-    <baidu-map
-      class="bm-view"
-      :center="center"
-      :zoom="zoom"
-      :scroll-wheel-zoom="true"
-      mapType="BMAP_HYBRID_MAP"
-      @ready="handler"
-    >
-      <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
-      <bm-point-collection
-        :points="points"
-        shape="BMAP_POINT_SHAPE_CIRCLE"
-        color="red"
-        size="BMAP_POINT_SIZE_NORMAL"
-        @click="clickHandler"
-      ></bm-point-collection>
-    </baidu-map>
+      <baidu-map
+          class="bm-view"
+          :center="center"
+          :zoom="zoom"
+          :scroll-wheel-zoom="true"
+          mapType="BMAP_HYBRID_MAP"
+          @ready="handler"
+      >
+        <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+        <bm-point-collection
+            :points="points"
+            shape="BMAP_POINT_SHAPE_CIRCLE"
+            color="red"
+            size="BMAP_POINT_SIZE_NORMAL"
+            @click="clickHandler"
+        ></bm-point-collection>
+      </baidu-map>
+    </div>
   </div>
-</div>
 </template>
 
 <script setup>
 import {ref, onMounted} from "vue";
-import { getList} from "@/api/robot";
-import map from "@/views/Map.vue";
+import {getList} from "@/api/robot";
 
-const pageNum = ref(12);
+const pageNum = ref(1);
 const pageSize = ref(50);
 const total = ref(0);
 const category_2 = ref("未知类型");
-
 const tableData = ref([]);
+const gps = ref({
+  lng: "",
+  lat: "",
+});
+const center = ref({lng: 0, lat: 0});
+const zoom = ref(3);
+const points = ref([]);
 
 //获取最大的pageNum
 const fetchData = () => {
@@ -43,49 +48,43 @@ const fetchData = () => {
   }).then((response) => {
     tableData.value = response.data.data.records;
     total.value = response.data.data.total;
+    pageNum.value = Math.ceil(total.value / pageSize.value);
   });
+};
+
+// 在地图上添加点
+const addPoints = () => {
+  const pointAll = [];
+  for (let i in tableData.value) {
+    pointAll.push({lng: tableData.value[i].lng, lat: tableData.value[i].lat});
+  }
+  points.value = pointAll;
+  console.log(points.value);
 };
 
 onMounted(() => {
   fetchData();
 });
 
-const gps = ref({
-  lng: "",
-  lat: "",
-});
 
-const center = ref({ lng: 0, lat: 0 });
-const zoom = ref(3);
-
-const handler = ({ BMap, map }) => {
+const handler = ({}) => {
   center.value.lng = tableData.value[0].lng;
   center.value.lat = tableData.value[0].lat;
   zoom.value = 15;
+  fetchData();
   addPoints();
 };
-
-const points = ref([]);
 
 const clickHandler = (e) => {
   alert(`单击点的坐标为：${e.point.lng}，${e.point.lat}`);
 };
-
-//在地图上添加点
-const addPoints = () => {
-  const pointAll = [];
-  for (let i in tableData.value) {
-    pointAll.push({ lng: tableData.value[i].lng, lat: tableData.value[i].lat });
-  }
-  points.value = pointAll;
-};
-
 </script>
 
 <style scoped>
 .content {
   margin: 50px;
 }
+
 .bm-view {
   width: 1400px;
   height: 600px;
